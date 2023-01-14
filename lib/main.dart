96a0fcf8b48e3f81ccd1028c6485dcee80f60185
faouz111 <faouz.elhoussine@gmail.com>
2023-01-14 01:06:13 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+ void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget{
+
+
+
+  const MyApp({Key? key}):super(key:key);
+
+  @override
+  Widget build(BuildContext context){
+   return const MaterialApp(
+     home:FacebookApp(),
+   );
+  }
+
+}
+
+
+class FacebookApp extends StatelessWidget {
+  const FacebookApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // ignore: prefer_const_constructors
+    return  Scaffold(
+     appBar: AppBar(
+      // ignore: prefer_const_constructors
+      title: Text("facebook" , style: TextStyle(color: Colors.white)),
+      centerTitle: true,
+      // ignore: prefer_const_constructors
+      leading: IconButton(icon: Icon(Icons.menu), onPressed: () {  },),
+
+     ),
+    );
+  }
+}
